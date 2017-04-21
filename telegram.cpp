fef7e3a7be7c7e1424b16e246da1db33bd77ad18
Devid Antonio Floni <d.filoni@ubuntu.com>
2017-04-21 21:36:22 +0200

    Close telegram-app issue #11
    https://github.com/yunit-io/telegram-app/issues/11

--- telegram.cpp
+++ telegram.cpp
@@ -1077,7 +1077,8 @@ void Telegram::createSharedKey(SecretChat *secretChat, BIGNUM *p, QByteArray gAO
 // error and internal managements
 void Telegram::onError(qint64 id, qint32 errorCode, const QString &errorText, const QString &functionName) {
     if(errorCode == 400) {
-        if(errorText == "ENCRYPTION_ALREADY_DECLINED") { /* This is an already declined chat, remove it from DB */
+        if(errorText == "ENCRYPTION_ID_INVALID" || /* We receive this when a chat has crashed */
+           errorText == "ENCRYPTION_ALREADY_DECLINED") { /* This is an already declined chat, remove it from DB */
             this->onMessagesDiscardEncryptionResult(id, true);
             return;
         }
